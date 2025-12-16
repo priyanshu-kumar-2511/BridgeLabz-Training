54f8255270daacfb53c90824463e011cb5b4e049
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-16 17:09:50 +0530

    [Priyanshu] : Print 3rd Program

---
+++ gcr-codebase/program prerequisite/Celsius_Fahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Celsius_Fahrenheit{
+       public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter Temperature in Celsius: ");
+       float a = sc.nextFloat();
+       float b = ((a * 9.0f/5.0f) + 32);
+       System.out.print("Fahrenheit : " + b);
+       }
+}
