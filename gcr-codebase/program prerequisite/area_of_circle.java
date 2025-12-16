06cee37b8d2d870fde7f732cff473410eac34009
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-16 17:28:13 +0530

    [Priyanshu] : Print 4th Program

---
+++ gcr-codebase/program prerequisite/area_of_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class area_of_circle{
+      public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Calculating the area of a circle: ");
+      System.out.print("Enter the radius of a circle : ");
+      float radius = sc.nextFloat();
+      float area = 3.14f * radius * radius;
+      System.out.print("Area of a Circle is " + area);
+      }
+}
