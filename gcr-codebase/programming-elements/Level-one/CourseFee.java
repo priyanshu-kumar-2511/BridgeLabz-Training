3b281039a4f68c0979a53f809d6c8026000da7e3
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-17 16:19:31 +0530

    [Priyanshu] : Adding University Discounted Fees of Student

---
+++ gcr-codebase/programming-elements/Level-one/CourseFee.java
@@ -0,0 +1,12 @@
+class CourseFee{
+    public static void main(String[] args) {
+        int fee = 125000;
+        int discount_percent = 10;
+
+        int discount = (fee * discount_percent) / 100;
+        int final_fee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount +
+                           " and final discounted fee is INR " + final_fee);
+    }
+}
