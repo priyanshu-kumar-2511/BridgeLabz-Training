8f34639ed5b6d36f6fb68098b896611772f7e7a6
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-17 14:52:21 +0530

    [Priyanshu] : Adding Kl to Miles Covertor Program

---
+++ gcr-codebase/programming-elements/Level-one/Convert.java
@@ -0,0 +1,8 @@
+class Convert{
+      public static void main(String[] args){
+      System.out.println("Distance of 1km is 1.6 miles");
+      double distance_in_km = 10.8;
+      double distance_in_miles = distance_in_km * 1.6;
+      System.out.println("The distance " + distance_in_km + " km in miles is " + distance_in_miles);
+      }
+}
