86cd8adf6410d8a38e76fe04a3a300c701ae807b
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-17 13:13:29 +0530

    [Priyanshu] : Adding Harry Program

---
+++ gcr-codebase/programming-elements/level-1/Harry.java
@@ -0,0 +1,8 @@
+class Harry{
+      public static void main(String[] args){
+	  System.out.println("Harry's Birth Year is 2000");
+	  System.out.println("Harry's age in 2024 is " + 2024-2000);
+	  System.out.println("Harry's Birth Year is 2000");
+	  System.out.println("Harry's age in 2024 is " + 2024-2000);
+	  }
+}	  
