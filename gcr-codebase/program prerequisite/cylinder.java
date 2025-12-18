54126d50101b3e5f88d325e7edae8e68b7a94dca
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-18 11:13:46 +0530

    [Priyanshu] : Adding Volume of Cylinder Program

---
+++ gcr-codebase/program prerequisite/cylinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class cylinder{
+      public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Calculating the volume of a cylinder");
+      System.out.print("Enter the radius of a cylinder : ");
+
+      float radius = sc.nextFloat();
+      System.out.print("Enter the height of a cylinder : ");
+
+      float height = sc.nextFloat();
+      float volume = 3.14f * radius * radius * height;
+
+      System.out.print("Volume of a cylinder is " + volume);
+   
+      sc.close();
+      }
+}
