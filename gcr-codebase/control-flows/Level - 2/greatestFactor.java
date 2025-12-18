f131328b9ff3cf4ae27b5416a39ecb9e929ba0d3
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-18 22:42:09 +0530

    [Priyanshu] : Adding Greatest Factor of a number Program

--- gcr-codebase/control-flows/Level - 2/greatestFactor.java
+++ gcr-codebase/control-flows/Level - 2/greatestFactor.java
@@ -8,9 +8,7 @@ class greatestFactor{
         System.out.print("Enter a number: ");
         int number = input.nextInt();  
 
-        
         int greatestFactor = 1;
-
         // checking the greatest factor of a number
         for (int i = number - 1; i >= 1; i--) {
             if (number % i == 0) {
