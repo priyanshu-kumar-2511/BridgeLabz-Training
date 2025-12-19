6c109ba4be4fb53489cfdae38c61232c5380f740
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-19 11:28:56 +0530

    [Priyanshu] : Adding Printing which days of a week Program

---
+++ gcr-codebase/control-flows/Level - 3/DayOfWeek.java
@@ -0,0 +1,26 @@
+class DayOfWeek{
+    public static void main(String[] args){
+
+        // taking input from command line
+        int m = Integer.parseInt(args[0]); // month
+        int d = Integer.parseInt(args[1]); // day
+        int y = Integer.parseInt(args[2]); // year
+
+        // Applying formulas 
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+        // displaying the result
+        switch (d0) {
+            case 0 -> System.out.println("Sunday");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+        }
+    }
+}
