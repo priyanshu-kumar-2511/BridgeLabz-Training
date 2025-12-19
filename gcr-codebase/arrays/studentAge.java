1e603fac1ead5ffeee5c206864f80d5c54c97b72
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-19 12:16:09 +0530

    [Priyanshu] : Adding Array of Student's Age Program

--- gcr-codebase/arrays/studentAge.java
+++ gcr-codebase/arrays/studentAge.java
@@ -8,6 +8,7 @@ class studentAge{
       int [] age = new int[10];
       for (int i=0;i<age.length;i++){
       // taking age input from user and checking Is student is eligible for vote or not
+          System.out.print("Enter the age of a student: ");
           age[i] = input.nextInt();
           if(age[i] >= 18){
              System.out.println("The student with the age "+ age[i]+" can vote.");
