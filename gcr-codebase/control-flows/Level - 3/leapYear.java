96a95478e7061996be47b09a2710229fcd784784
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-19 08:40:29 +0530

    [Priyanshu] : Adding Leap Year Program

---
+++ gcr-codebase/control-flows/Level - 3/leapYear.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class leapYear{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        // Taking a year from user
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Checking year is valid for Gregorian calendar
+        if (year >= 1582) {
+        // checking yaer is a leap year or not        
+            if (year % 400 == 0) {
+                System.out.println(year+" is a Leap Year");
+            } 
+            else if (year % 100 == 0) {
+                System.out.println(year+" is not a Leap Year");
+            } 
+            else if (year % 4 == 0) {
+                System.out.println(year+" is a Leap Year");
+            } 
+            else {
+                System.out.println(year+" is not a Leap Year");
+            }
+        }
+        input.close();
+    }
+}
