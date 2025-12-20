409cb486e19b59396aa12a8727817e41e1f4aa36
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-20 13:09:40 +0530

    [Priyanshu] : Added Maximum Number Of Handshakes Possible Program

--- gcr-codebase/methods/Level -1/MaximumHandshakes.java
+++ gcr-codebase/methods/Level -1/MaximumHandshakes.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 public class MaximumHandshakes{
+      // Creating method for calculating the maximum number of handshakes posiible
        public static double calculatingMaximumHandshakes(double number){
 	          double numberOfHandshakes = (number *(number - 1))/2;
-              return numberOfHandshakes;
+                return numberOfHandshakes;
         }
    
        public static void main(String[] args){
               Scanner input = new Scanner(System.in);
+              // taking number of students from user
               System.out.print("Enter the Number of Students : ");
               double numberOfStudents = input.nextDouble();
               
+              // calling method for calculating the maximum number of handshakes posiible 
               double numberOfHandshakes = MaximumHandshakes.calculatingMaximumHandshakes(numberOfStudents);
+              // printing the results
               System.out.print("The Maximum Number of Possible Handshakes is "+ numberOfHandshakes);
         }
 }		
