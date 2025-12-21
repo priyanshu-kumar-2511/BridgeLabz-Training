9d795e0ceeb873798e381672cabc108d2c581b0a
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-21 13:17:20 +0530

    [Priyanshu] : Added Factors of a Number Program

---
+++ gcr-codebase/methods/Level - 2/FactorsOfANumber.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+public class FactorsOfANumber {
+
+    // creating method to find factors
+    public static int[] findFactors(int number) {
+
+        // First loop: count factors
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+
+        // to store factors in array
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index] = i;
+                index++;
+            }
+        }
+        return factors;
+    }
+
+    // creating method to find sum of factors
+    public static int findSum(int[] factors) {
+        int sum = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sum += factors[i];
+        }
+        return sum;
+    }
+
+    // creating method to find product of factors
+    public static long findProduct(int[] factors) {
+        long product = 1;
+        for (int i = 0; i < factors.length; i++) {
+            product *= factors[i];
+        }
+        return product;
+    }
+
+    // creating method to find sum of squares of factors
+    public static double findSumOfSquares(int[] factors) {
+        double sum = 0;
+        for (int i = 0; i < factors.length; i++) {
+            sum += Math.pow(factors[i], 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // taking input from user
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int[] factors = findFactors(number);
+
+        System.out.print("Factors: ");
+        for (int i = 0; i < factors.length; i++) {
+            System.out.print(factors[i] + " ");
+        }
+
+        System.out.println("\nSum of factors: " + findSum(factors));
+        System.out.println("Product of factors: " + findProduct(factors));
+        System.out.println("Sum of squares of factors: " + findSumOfSquares(factors));
+
+        input.close();
+    }
+}
