310bb92ff35eff61e7564a9ce81359b7f9362a67
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-22 20:37:24 +0530

    [Priyanshu] : Added Split Text Without Split Method Program

---
+++ gcr-codebase/strings/Level - 2/SplitTextWithoutSplit.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+public class SplitTextWithoutSplit {
+
+    // create method to find string length
+    public static int findLength(String text) {
+        int count = 0;
+
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+
+    // create method split text into words using charAt()
+    public static String[] splitTextManually(String text) {
+
+        int length = findLength(text);
+
+        int wordCount = 1;
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) == ' ') {
+                wordCount++;
+            }
+        }
+		
+        int[] spaceIndexes = new int[wordCount - 1];
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) == ' ') {
+                spaceIndexes[index++] = i;
+            }
+        }
+
+        String[] words = new String[wordCount];
+        int start = 0;
+        for (int i = 0; i < spaceIndexes.length; i++) {
+            words[i] = text.substring(start, spaceIndexes[i]);
+            start = spaceIndexes[i] + 1;
+        }
+
+        words[wordCount - 1] = text.substring(start);
+        return words;
+    }
+
+    // create method compare two String arrays
+    public static boolean compareStringArrays(String[] arr1, String[] arr2) {
+
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (!arr1[i].equals(arr2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String text = sc.nextLine();
+
+        String[] manualWords = splitTextManually(text);
+
+        String[] builtInWords = text.split(" ");
+
+        boolean result = compareStringArrays(manualWords, builtInWords);
+
+        System.out.println("\nWords using user-defined method:");
+        for (String word : manualWords) {
+            System.out.println(word);
+        }
+
+        System.out.println("\nWords using split() method:");
+        for (String word : builtInWords) {
+            System.out.println(word);
+        }
+
+        System.out.println("\nAre both results equal? " + result);
+        sc.close();
+    }
+}
