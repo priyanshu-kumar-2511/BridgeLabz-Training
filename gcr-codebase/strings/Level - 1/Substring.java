cc974b54172834cb2f1608331e7efb9d5e98fc3d
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-22 14:17:44 +0530

    [Priyanshu] : Added Compare substring using charAt and substring method Program

---
+++ gcr-codebase/strings/Level - 1/Substring.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class Substring {
+
+    // creating method substring using charAt()
+    public static String createSubstringUsingCharAt(String text, int start, int end) {
+        String result = "";
+
+        for (int i = start; i < end; i++) {
+            result += text.charAt(i);
+        }
+
+        return result;
+    }
+
+    // creating method compare two strings using charAt()
+    public static boolean compareStringsUsingCharAt(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String text = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        // Substring using charAt()
+        String manualSubstring = createSubstringUsingCharAt(text, start, end);
+
+        // Substring using built-in method
+        String builtInSubstring = text.substring(start, end);
+
+        // Compare both substrings
+        boolean areEqual = compareStringsUsingCharAt(manualSubstring, builtInSubstring);
+
+        // Display results
+        System.out.println("\nSubstring using charAt(): " + manualSubstring);
+        System.out.println("Substring using substring(): " + builtInSubstring);
+        System.out.println("Are both substrings equal? " + areEqual);
+
+        sc.close();
+    }
+}
