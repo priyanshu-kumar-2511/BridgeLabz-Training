331c968cc1abfe0162f78fdce65868ab52542987
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-23 16:46:50 +0530

    [Priyanshu] : Added Reverse String method program

---
+++ gcr-codebase/extra/extra - java string/ReverseString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ReverseString {
+
+    static String reverseString(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reverseString(input));
+    }
+}
