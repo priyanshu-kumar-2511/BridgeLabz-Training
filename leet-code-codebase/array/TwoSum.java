172d4611abf914ad6086fb4b8c0c1891f0e0852f
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-24 15:11:56 +0530

    [Priyanshu] : Added Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target program

--- leet-code-codebase/array/TwoSum.java
+++ leet-code-codebase/array/TwoSum.java
@@ -1,4 +1,13 @@
-class Name
-{
-
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+    }
 }
