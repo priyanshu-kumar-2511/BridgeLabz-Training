2d13660a295c0b1ce3160eb0326e2c481956ded3
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-27 23:45:55 +0530

    [Priyanshu] : Added Student and PostgraduateStudent classes

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/PostgraduateStudent.java
@@ -0,0 +1,28 @@
+package com.constructor.level1;
+
+public class PostgraduateStudent extends Student {
+
+    String specialization;
+
+    public PostgraduateStudent(int rollNumber, String name, double cgpa, String specialization) {
+        super(rollNumber, name, cgpa);
+        this.specialization = specialization;
+    }
+
+    public void display() {
+        System.out.println("Roll Number    : " + rollNumber);
+        System.out.println("Name           : " + name);
+        System.out.println("CGPA           : " + getCGPA());
+        System.out.println("Specialization : " + specialization);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        PostgraduateStudent p =
+                new PostgraduateStudent(130, "Priyanshu", 8.5, "DS");
+
+        p.display();
+        p.setCGPA(9.1);
+        System.out.println("Updated CGPA: " + p.getCGPA());
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Student.java
@@ -0,0 +1,26 @@
+package com.constructor.level1;
+
+public class Student {
+
+    public int rollNumber;
+    protected String name;
+    private double cgpa;
+
+    public Student(int rollNumber, String name, double cgpa) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+
+    public double getCGPA() {
+        return cgpa;
+    }
+
+    public void setCGPA(double cgpa) {
+        if (cgpa >= 0 && cgpa <= 10) {
+            this.cgpa = cgpa;
+        } else {
+            System.out.println("Invalid CGPA");
+        }
+    }
+}
