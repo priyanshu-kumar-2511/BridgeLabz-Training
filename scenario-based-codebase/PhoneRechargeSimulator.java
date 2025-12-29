99cfe62edcc523f8a60cb9e216c7c7a56312dade
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-29 21:59:34 +0530

    [Priyanshu] : Added Phone Recharge Simulator Prpgram

--- scenario-based-codebase/PhoneRechargeSimulator.java
+++ scenario-based-codebase/PhoneRechargeSimulator.java
@@ -7,41 +7,60 @@ Take the user's mobile operator and amount.
 */
 
 import java.util.Scanner;
-public class PhoneRechargeSimulator{
-    public static void main(String[] args){
-	
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Mobile operator: ");
-	System.out.println("1. Airtle");
-	System.out.println("2. Vi");
-	System.out.println("3. Jio");
-	
-	System.out.print("Choose Your Operator: ");
-	int choose = sc.nextInt();
-	int balance;
-	switch(choose){
-		System.out.println(" ");
-		case 1 : 
-		        System.out.println("Rs 49 : Unlimited Data for 1 Days");
-		        System.out.println("Rs 33 : 2gb data for 1 Days");
-				System.out.print("Enter the amount: ");
-				balance = sc.nextInt();
-				System.out.println("Recharge of "+ balance +" Succesfully");
-				break;
-		case 2 : 
-                System.out.println("Rs 51 : Unlimited Data for 1 Days");
-		        System.out.println("Rs 33 : 2gb data for 1 Days");
-				System.out.print("Enter the amount: ");
-				balance = sc.nextInt();
-				System.out.println("Recharge of "+ balance +" Succesfully");
-				break;	
-        case 3 : 
-                System.out.println("Rs 42 : Unlimited Data for 1 Days");
-		        System.out.println("Rs 21 : 2gb data for 1 Days");
-				System.out.print("Enter the amount: ");
-				balance = sc.nextInt();
-				System.out.println("Recharge of Rs "+ balance +" Succesfully Done");
-				break;	
+
+public class PhoneRechargeSimulator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int balance = 0;
+        int choose;
+        char repeat = 'y';
+
+        do {
+            System.out.println("\nMobile operator:");
+            System.out.println("1. Airtel");
+            System.out.println("2. Vi");
+            System.out.println("3. Jio");
+
+            System.out.print("Choose Your Operator: ");
+            choose = sc.nextInt();
+
+            switch (choose) {
+
+                case 1:
+                    System.out.println("Rs 49 : Unlimited Data for 1 Day");
+                    System.out.println("Rs 33 : 2GB Data for 1 Day");
+                    break;
+
+                case 2:
+                    System.out.println("Rs 51 : Unlimited Data for 1 Day");
+                    System.out.println("Rs 33 : 2GB Data for 1 Day");
+                    break;
+
+                case 3:
+                    System.out.println("Rs 42 : Unlimited Data for 1 Day");
+                    System.out.println("Rs 21 : 2GB Data for 1 Day");
+                    break;
+
+                default:
+                    System.out.println("Invalid Operator");
+                    continue;
+            }
+
+            System.out.print("Enter the amount: ");
+            int amount = sc.nextInt();
+            balance += amount;
+
+            System.out.println("Recharge of Rs " + amount + " Successful");
+            System.out.println("Current Balance: Rs " + balance);
+
+            System.out.print("Do you want to recharge again? (y/n): ");
+            repeat = sc.next().toLowerCase().charAt(0);
+
+        } while (repeat == 'y');
+
+        System.out.println("Thank you for using Recharge Simulator.");
+        sc.close();
     }
-    }	
-}	
+}
+
