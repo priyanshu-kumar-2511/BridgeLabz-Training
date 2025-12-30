862bcd20032842d40c1cf46248ee50cdb425857f
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2025-12-30 23:14:38 +0530

    [Priyanshu] : Added String problem no. 67 Add Binary

---
+++ leet-code-codebase/string/AddBinary.java
@@ -0,0 +1,30 @@
+class AddBinary {
+    public String addBinary(String a, String b) {
+
+        StringBuilder result = new StringBuilder();
+
+        int i = a.length() - 1;   // pointer for a
+        int j = b.length() - 1;   // pointer for b
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+
+            result.append(sum % 2);   // current binary digit
+            carry = sum / 2;          // carry for next bit
+        }
+
+        return result.reverse().toString();
+    }
+}
