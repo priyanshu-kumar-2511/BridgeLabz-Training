34a9661123778eb9f7c3704d26499fbf2e4fad69
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-01-04 22:25:28 +0530

    [Priyanshu] : Added Singly Linked List: Student Record Management

--- dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/Main.java
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/singlylinkedlist/studentrecordmanagement/Main.java
@@ -1,5 +1,18 @@
 package com.linkedlist.singlylinkedlist.studentrecordmanagement;
 
+/*1. Singly Linked List: Student Record Management
+Problem Statement: Create a program to manage student records using a singly linked list. Each node will store information about a student, including their Roll Number, Name, Age, and Grade. Implement the following operations:
+Add a new student record at the beginning, end, or at a specific position.
+Delete a student record by Roll Number.
+Search for a student record by Roll Number.
+Display all student records.
+Update a student's grade based on their Roll Number.
+Hint:
+Use a singly linked list where each node contains student information and a pointer to the next node.
+The head of the list will represent the first student, and the last node’s next pointer will be null.
+Update the next pointers when inserting or deleting nodes.
+*/
+
 public class Main{
     public static void main(String[] args) {
 
