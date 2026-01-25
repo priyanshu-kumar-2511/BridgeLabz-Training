639702ae1efc7e5d6cffd5dc591a3f17c2f57709
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-01-25 22:21:46 +0530

    [Priyanshu] : Added Merge Two Maps

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcept/src/com/collections/mapinterface/mergetwomaps/MergeTwoMaps.java
@@ -0,0 +1,43 @@
+package com.collections.mapinterface.mergetwomaps;
+
+/*4. Merge Two Maps
+Merge two maps such that if a key exists in both, sum their values.
+Example:
+Map1: {A=1, B=2}, Map2: {B=3, C=4} → Output: {A=1, B=5, C=4}.
+*/
+
+import java.util.*;
+
+public class MergeTwoMaps {
+
+    public static Map<String, Integer> mergeMaps(
+            Map<String, Integer> map1,
+            Map<String, Integer> map2) {
+
+        Map<String, Integer> result = new HashMap<>(map1);
+
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+
+            result.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+        return result;
+    }
+
+    // main method
+    public static void main(String[] args) {
+
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+
+        Map<String, Integer> merged = mergeMaps(map1, map2);
+
+        System.out.println(merged);
+    }
+}
+
