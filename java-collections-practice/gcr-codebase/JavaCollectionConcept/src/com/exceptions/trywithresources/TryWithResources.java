ba0a8bc5ba20c435aa47febdcf3ca11339937048
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-01-26 17:40:51 +0530

    [Priyanshu] : Added try-with-resources (Auto-closing Resources)

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcept/src/com/exceptions/trywithresources/TryWithResources.java
@@ -0,0 +1,35 @@
+package com.exceptions.trywithresources;
+
+/*5. try-with-resources (Auto-closing Resources)
+💡 Problem Statement:
+Write a Java program that reads the first line of a file named "info.txt" using BufferedReader.
+Use try-with-resources to ensure the file is automatically closed after reading.
+Handle any IOException that may occur.
+Expected Behavior:
+If the file exists, print its first line.
+If the file does not exist, catch IOException and print "Error reading file".
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+
+            String firstLine = br.readLine();
+
+            if (firstLine != null) {
+                System.out.println(firstLine);
+            } else {
+                System.out.println("File is empty");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
