f30c955c76f13c297cfd666c08f2b38a1f1882c7
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-01-26 17:38:34 +0530

    [Priyanshu] : Added Multiple Catch Blocks

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcept/src/com/exceptions/multiplecatch/MultipleCatch.java
@@ -0,0 +1,48 @@
+package com.exceptions.multiplecatch;
+
+/*4. Multiple Catch Blocks
+💡 Problem Statement:
+Create a Java program that performs array operations.
+Accept an integer array and an index number.
+Retrieve and print the value at that index.
+Handle the following exceptions:
+ArrayIndexOutOfBoundsException if the index is out of range.
+NullPointerException if the array is null.
+Expected Behavior:
+If valid, print "Value at index X: Y".
+If the index is out of bounds, display "Invalid index!".
+If the array is null, display "Array is not initialized!".
+*/
+
+import java.util.Scanner;
+
+public class MultipleCatch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Integer[] array = null;  
+
+        try {
+
+            array = new Integer[]{10, 20, 30, 40, 50};
+
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            int value = array[index];
+
+            System.out.println("Value at index " + index + ": " + value);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+
+        } finally {
+            sc.close();
+        }
+    }
+}
