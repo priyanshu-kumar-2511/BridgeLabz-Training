b9617753fa46b92ff4eec706651a47fd7c63e610
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-02-01 01:09:47 +0530

    [Priyanshu] : Added Temperature Alert System

--- java8-programmming-practice/gcr-codebase/Java8ProgrammmingConcept/src/com/functionalinterface/funtionalinterfaces/temperaturealertsystem/Main.java
+++ java8-programmming-practice/gcr-codebase/Java8ProgrammmingConcept/src/com/functionalinterface/funtionalinterfaces/temperaturealertsystem/Main.java
@@ -5,17 +5,20 @@ package com.functionalinterface.funtionalinterfaces.temperaturealertsystem;
 ○ Task: Use Predicate<Double> functional interface.
 */
 import java.util.function.Predicate;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    	Scanner sc = new Scanner(System.in);
         double threshold = 37.5;
 
         // Predicate to check if temperature exceeds threshold
         Predicate<Double> isHighTemperature = temp -> temp > threshold;
 
-        double currentTemp = 38.2;
+        System.out.println("Enter the current temperature: ");
+        double currentTemp = sc.nextDouble();
 
         if (isHighTemperature.test(currentTemp)) {
             System.out.println("ALERT: Temperature is too high!");
