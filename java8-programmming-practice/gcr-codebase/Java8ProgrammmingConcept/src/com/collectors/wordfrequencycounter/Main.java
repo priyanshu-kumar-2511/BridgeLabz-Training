38af60af7de6b7236ba4eb565f43dc50b9ddde4b
priyanshu-kumar-2511 <kumarpriyanshu77828@gmail.com>
2026-02-01 21:09:33 +0530

    [Priyanshu] : Added Word Frequency Counter Program

--- java8-programmming-practice/gcr-codebase/Java8ProgrammmingConcept/src/com/collectors/wordfrequencycounter/Main.java
+++ java8-programmming-practice/gcr-codebase/Java8ProgrammmingConcept/src/com/collectors/wordfrequencycounter/Main.java
@@ -1,5 +1,22 @@
 package com.collectors.wordfrequencycounter;
 
+/*2. Word Frequency Counter
+○ Scenario: Analyze a paragraph for word occurrence.
+○ Task: Use Collectors.toMap().*/
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+        String paragraph = "Java is powerful. Java is fast, and Java is scalable!";
+
+        Map<String, Integer> wordFrequency = Arrays.stream(paragraph.toLowerCase().replaceAll("[^a-z ]", "").split("\\s+")                 )
+                .collect(Collectors.toMap(word -> word,word -> 1,Integer::sum));
+
+        wordFrequency.forEach((word, count) -> System.out.println(word + " → " + count));
+    }
 }
